cf94740c915240ebe55be48945a27e4232742302
Henna Niemi <hennasniemi@gmail.com>
2021-03-29 20:11:06 +0300

    allpersons works

--- qt_frontend/mainwindow.cpp
+++ qt_frontend/mainwindow.cpp
@@ -26,11 +26,30 @@ void MainWindow::on_getAllPersons_clicked()
     ptrAllPersonsManager = new QNetworkAccessManager(this);
 
     connect(ptrAllPersonsManager, SIGNAL(finished (QNetworkReply*)),
-    this, SLOT(getBookSlot(QNetworkReply*)));
+    this, SLOT(allPersonsSlot(QNetworkReply*)));
     ptrAllPersonsReply = ptrAllPersonsManager->get(request);
 }
 
-void MainWindow::allPersonsSlot(QNetworkReply *reply)
+void MainWindow::allPersonsSlot(QNetworkReply*reply)
 {
+    QByteArray response_data = reply->readAll();
+    qDebug()<<response_data;
+    if(response_data.compare("-4078")==0){
+        ui->label_result->setText("Virhe tietokantayhteydessä");
+    }
+    else{
+        QJsonDocument json_doc = QJsonDocument::fromJson(response_data);
+        QJsonArray json_array = json_doc.array();
+        QString persons;
+        foreach(const QJsonValue &value,json_array){
+            QJsonObject json_object=value.toObject();
+            persons+=QString::number((json_object["id_person"].toInt()))
+                    +" "+json_object["fname"].toString()+"      "+json_object["lname"].toString()+" "+QString::number((json_object["money"].toDouble()))+"\r\n";
+        }
+        ui->label_result->setText(persons);
+    }
 
+    ptrAllPersonsReply->deleteLater();
+    reply->deleteLater();
+    ptrAllPersonsManager->deleteLater();
 }
